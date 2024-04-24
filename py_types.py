0f062a9c9786ab6fb97fb8a26370564333559984
Tomek <29851015+bxtom@users.noreply.github.com>
2024-04-24 21:42:56 +0200

    struktury

--- py_types.py
+++ py_types.py
@@ -2,3 +2,41 @@ print("To jest podwójny" + "cytowany ciąg")
 print("To 'jest' podwójny")
 i = (9 - 12) * (3 + 6)
 print(i)
+ziemia_jest_plaska = True
+print(ziemia_jest_plaska)
+
+slownik = { 
+  "name": "Twoje imię", 
+  "age": 19
+}
+print(slownik)
+
+lista = ["banan", "chleb", "mleko owsiane"]
+print(lista)
+
+lista_liczb = [35, 8, 12, 6]
+print(lista_liczb)
+
+lista_mieszana = [35, 'mleko', 12, 'cukier', 36.6]
+print(lista_mieszana)
+
+zestaw = {"apple", "banana", "cherry"}
+print(zestaw)
+
+krotka = ("apple", "banana", "cherry")
+print(krotka)
+
+zestaw_mieszany = {42, 3.14, "apple", ("banana", "cherry")}
+print(zestaw_mieszany)
+
+lista_slownikow = [
+  {
+    "name": "Twoje imię",
+    "age": 19
+  },
+  {
+    "name": "Inne imię",
+    "wiek": 65
+  }
+]
+print(lista_slownikow)
